c0353991ba9e2eb7346dfaac24ee3a863f7a9350
blevm <mjblevins7@gmail.com>
2018-07-01 12:48:49 -0400

    mb solution

--- src/Tier1.js
+++ src/Tier1.js
@@ -14,13 +14,29 @@ export default class Tier1 extends Component {
     }
   }
 
+  handleClick = (event) => {
+    if (event.target.className === 'tier1') {
+      console.log('tier 1 handler running');
+      this.setState({color: getRandomColor()}, () => this.setState({
+        childColor: getReducedColor(this.state.color)
+      }))
+    }
+  }
+
+  handleNestedClick = (event) => {
+  if (event.target.className === 'tier2') {
+    console.log(`handling nested click in tier 1`, event.target.className);
+    this.setState({
+      childColor: getRandomColor()
+    })
+  }
+}
+
   render() {
-    // hard coded color values have been added below, though they won't be
-    // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={"#0F0"} />
-        <Tier2 color={"#0FF"} />
+      <div onClick={this.handleClick} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
+        <Tier2 color={this.state.childColor} handleNestedClick={this.handleNestedClick} />
+        <Tier2 color={this.state.childColor} handleNestedClick={this.handleNestedClick} />
       </div>
     )
   }
